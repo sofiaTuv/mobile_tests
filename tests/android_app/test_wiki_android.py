a288a52b22353e456aa1cec1d7d16fd756d20521
Sofia Tuvykina <hdfaa.56@gmail.com>
2023-12-14 19:38:39 +0300

    tests android

---
+++ tests/android_app/test_wiki_android.py
@@ -0,0 +1,25 @@
+from allure_commons._allure import step
+from appium.webdriver.common.appiumby import AppiumBy
+from selene import browser, have
+
+
+def test_search_appium():
+    with step('Type search "Appium"'):
+        browser.element((AppiumBy.ACCESSIBILITY_ID, "Search Wikipedia")).click()
+        browser.element((AppiumBy.ID, "org.wikipedia.alpha:id/search_src_text")).type('Appium')
+
+    with step('Verify content found "Appium"'):
+        results = browser.all((AppiumBy.ID, 'org.wikipedia.alpha:id/page_list_item_title'))
+        results.should(have.size_greater_than(0))
+        results.first.should(have.text('Appium'))
+
+
+def test_search_google():
+    with step('Type search "Google"'):
+        browser.element((AppiumBy.ACCESSIBILITY_ID, "Search Wikipedia")).click()
+        browser.element((AppiumBy.ID, "org.wikipedia.alpha:id/search_src_text")).type('Google')
+
+    with step('Verify content found "Google"'):
+        results = browser.all((AppiumBy.ID, 'org.wikipedia.alpha:id/page_list_item_title'))
+        results.should(have.size_greater_than(0))
+        results.first.should(have.text('Google'))
