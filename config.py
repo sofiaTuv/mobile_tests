86b5913472487172cd6d4f4319c6c108d4497130
Sofia Tuvykina <hdfaa.56@gmail.com>
2023-12-14 19:39:58 +0300

    added

---
+++ config.py
@@ -0,0 +1,5 @@
+import os
+
+context = os.getenv('context', 'bstack')
+bstack_userName = os.getenv('bstack_userName', 'sofiatuvykina_pkVzJY')
+bstack_accessKey = os.getenv('bstack_accessKey', '4UNCy5QxoqBxy4yLLJag')
